f62cdd610196585e854fa67ae9b4c2f647e714a4
jeongmyeong <kimbym23@gmail.com>
2023-04-04 21:09:15 +0900

    initialize

---
+++ src/main/java/com/example/bootintegrationchanneldemo/BootIntegrationChannelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootintegrationchanneldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootIntegrationChannelDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootIntegrationChannelDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootintegrationchanneldemo/BootIntegrationChannelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootintegrationchanneldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootIntegrationChannelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
